2ba75d21bdd1b94b38d5d9a3460835096cd9ee66
Andrew Dinh <andrewdinh74@yahoo.com>
2025-12-09 15:46:24 -0800

    Create an Item class model and a fake db

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
+fake_db = []
+
+class Item(BaseModel):
+    name: str
+    price: float
+    in_stock: bool = True
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    fake_db.append(item.model_dump())
+    return item)
 
 @app.get("/")
 async def root():
